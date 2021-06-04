6e515215bb18389e5adbd3296c30eadac05b7789
ChiterP <63542970+ChiterP@users.noreply.github.com>
2021-06-04 09:25:28 +0300

    Initial Commit

---
+++ СolorMatchingUIKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  СolorMatchingUIKit
+//
+//  Created by 18316409 on 04.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ СolorMatchingUIKit/_olorMatchingUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _olorMatchingUIKitApp.swift
+//  СolorMatchingUIKit
+//
+//  Created by 18316409 on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct _olorMatchingUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
